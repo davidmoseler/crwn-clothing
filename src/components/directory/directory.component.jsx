c229dccd358871185fdd6675f98000f36a0a309e
David Moseler <davidmoseler@protonmail.com>
2020-11-26 05:33:28 -0300

    Refactoring and restyling

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import "./directory.styles.scss"
+import "../menu-item/menu-item.component.jsx"
+
+export class Directory extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            sections: []
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({id, title, imageUrl, size}) => {
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}></MenuItem>
+                    })
+                }
+            </div>
+        )
+    }
+}
